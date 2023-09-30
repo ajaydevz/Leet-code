da37d48f13342b763ec3cb97929cb19f5b184c31
Ajay Dev K <133864104+ajaydevz@users.noreply.github.com>
2023-09-30 10:22:18 +0530

    Time: 19 ms (85.91%), Space: 13 MB (100%) - LeetHub

---
+++ 1542-consecutive-characters/1542-consecutive-characters.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxPower(self, s):
+        max_count = 1 
+        current_count = 1  
+
+        for i in range(1, len(s)):  
+            if s[i] == s[i-1]:
+                current_count += 1
+                max_count = max(max_count, current_count)
+            else:
+                current_count = 1
+
+        return max_count
+
+
+
+
+
+
+
