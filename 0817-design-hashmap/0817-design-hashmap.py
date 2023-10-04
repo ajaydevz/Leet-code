48d953d43f55def600e4465dbbcb66d34d810bbe
Ajay Dev K <133864104+ajaydevz@users.noreply.github.com>
2023-10-04 12:15:30 +0530

    Time: 218 ms (56.68%), Space: 20.1 MB (58.72%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -0,0 +1,38 @@
+class ListNode:
+    def __init__(self, key=-1, val=-1,next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+class MyHashMap:
+
+    def __init__(self):
+        self.map = [ListNode() for i in range(1000)]
+
+    def hash(self, key):
+        return key % len(self.map)
+
+    def put(self, key: int, value: int) -> None:
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.val = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        cur = self.map[self.hash(key)].next
+        while cur:
+            if cur.key == key:
+                return cur.val
+            cur = cur.next
+        return -1
+        
+    def remove(self, key: int) -> None:
+        cur = self.map[self.hash(key)]
+        while cur and cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
