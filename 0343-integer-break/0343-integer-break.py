69788fc3402f05de152e66a5082412692b699d53
Ajay Dev K <133864104+ajaydevz@users.noreply.github.com>
2023-10-06 10:21:39 +0530

    Time: 43 ms (39.16%), Space: 16.1 MB (97.72%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.py
@@ -0,0 +1,3 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        return (n - 1) if n <= 3 else (3 ** (n // 3)) if n % 3 == 0 else (3 ** (n // 3 - 1) * 4) if n % 3 == 1 else (3 ** (n // 3) * 2)
