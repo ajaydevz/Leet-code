63344b02e7745d1e47d2817a977d8bfd7e5947d9
Ajay Dev K <133864104+ajaydevz@users.noreply.github.com>
2023-10-10 10:24:44 +0530

    Time: 45 ms (10.33%), Space: 16.3 MB (50.68%) - LeetHub

--- 1660-thousand-separator/1660-thousand-separator.py
+++ 1660-thousand-separator/1660-thousand-separator.py
@@ -1,4 +1,15 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
-        rep  = "{:,}".format(n)       
-        return rep.replace(",", ".")   
+        n = str(n)  # Convert the integer to a string
+        length = len(n)  # Calculate the length of the string
+
+        if length <= 3:
+            return n  # If the number has 3 or fewer digits, no need for separators
+
+        result = []
+        for i in range(length):
+            if i > 0 and (length - i) % 3 == 0:
+                result.append('.')  # Add a separator before every group of 3 digits
+            result.append(n[i])
+
+        return ''.join(result)
